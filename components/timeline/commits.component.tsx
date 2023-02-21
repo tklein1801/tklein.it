9ab488a2935168414cba96f4cd494c31f43d7e31
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-02-21 17:01:17 +0100

    fix: Improve mobile visualization

--- components/timeline/commits.component.tsx
+++ components/timeline/commits.component.tsx
@@ -83,19 +83,77 @@ export const Day: React.FC<DayProps> = ({ day, highestContribution }) => {
 };
 
 export const CommitsTimeline: React.FC<CommitTimelineProps> = ({ contributions }) => {
+  const [mobileView, setMobileView] = React.useState(false);
+
   const highestContribution = React.useMemo(() => {
     return contributions.contributionCalendar.weeks
       .map((week) => week.contributionDays.map((day) => day.contributionCount))
       .flat(1)
       .reduce((a, b) => Math.max(a, b));
   }, [contributions]);
+
+  const displayedWeeks: ContributionWeek[] = React.useMemo(() => {
+    const { weeks } = contributions.contributionCalendar;
+    return mobileView ? weeks.slice(weeks.length / 2, weeks.length) : weeks;
+  }, [mobileView]);
+
+  const handleWindowResize = () => {
+    setMobileView(getWindowDimensions(window).breakpoint === 'xs');
+  };
+
+  React.useEffect(() => {
+    handleWindowResize();
+
+    window.addEventListener('resize', () => {
+      handleWindowResize();
+    });
+
+    return () => {
+      window.removeEventListener('resize', () => {
+        handleWindowResize();
+      });
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
-      <div className={styles.grid}>
-        {contributions.contributionCalendar.weeks.map((week, idx) => (
+      <div
+        className={styles.grid}
+        style={{
+          gridTemplateColumns: `repeat(${displayedWeeks.length}, 1fr)`,
+        }}
+      >
+        {displayedWeeks.map((week, idx) => (
           <Week key={'week-' + idx} week={week} highestContribution={highestContribution} />
         ))}
       </div>
     </div>
   );
 };
+
+export type Breakpoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+export type DeviceSize = 'small' | 'medium' | 'wide';
+
+export function getBreakpoint(width: number): Breakpoint {
+  if (width >= 1536) {
+    return 'xl';
+  } else if (width >= 1200) {
+    return 'lg';
+  } else if (width >= 900) {
+    return 'md';
+  } else if (width >= 600) {
+    return 'sm';
+  } else {
+    return 'xs';
+  }
+}
+
+export function getWindowDimensions(window: Window) {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+    breakpoint: getBreakpoint(width),
+  };
+}

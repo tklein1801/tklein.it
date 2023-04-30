60aa1fa6a5950d209e1c762bd4e51c0aca92a67a
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-04-30 17:46:08 +0200

    chore: Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 import '../styles/globals.css';
+import React from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />
+      <Analytics />
+    </React.Fragment>
+  );
 }
